1b9ae0d417361275faf465ef17fe43fc7d5dbc2a
danghuyphu1709 <contact.danghuyphu1709@gmail.com>
2024-06-10 10:28:54 +0700

    demo feature dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import Button from "@mui/material/Button";
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
 import HomeIcon from "@mui/icons-material/Home";
+import Typography from "@mui/material/Typography";
 
 
 function App() {
   return (
     <>
+     <Typography variant="body2" gutterBottom color="text.success">
+        body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+        neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+        quasi quidem quibusdam.
+      </Typography>
       <div>Đặng Huy Phú</div>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>

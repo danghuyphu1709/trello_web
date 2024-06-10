4e8097a8cbac97513ca10f415822b13071a314c2
danghuyphu1709 <contact.danghuyphu1709@gmail.com>
2024-06-10 20:12:56 +0700

    create layout basic

--- src/theme.js
+++ src/theme.js
@@ -1,23 +1,30 @@
-import { createTheme } from '@mui/material/styles';
-import { green, red } from '@mui/material/colors';
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+import { cyan, deepOrange, orange, teal } from "@mui/material/colors";
 
 // Create a theme instance.
-const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    text: {
-      success: red.A400,
-    }
+const theme = extendTheme({
+  trello:{
+    appHeaderHeight : '88px',
+    appGridMenu : '20%',
+    appGridContent: '80%',
   },
+  colorSchemes: {
+    light: {
+      palette: {
+          primary: teal,
+          secondary: deepOrange
+      },
+      spacing: (factor) => `${0.25 * factor}rem`,
+    },
+    dark: {
+      palette: {
+          primary: cyan,
+          secondary: orange
+      },
+      spacing: (factor) => `${0.25 * factor}rem`,
+    },
+  },
+  // ...other properties
 });
 
 export default theme;
